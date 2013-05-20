64973ed9c58ee4787694c761edaba4ca05fa79cc
Ph3nol <cedric.dugat@gmail.com>
2013-05-20 14:14:43 +0200

    Cleaning

--- server/main.js
+++ server/main.js
@@ -1,11 +1,3 @@
-Meteor.publish('test_subscription', function() {
-  var language = get_http_header(this, 'ACCEPT_LANGUAGE'),
-      client   = get_http_remote_ip(this)
-  ;
-
-  console.log('Client', client, 'requested language preference', language);
-});
-
 Meteor.publish('categories', function () {
   return Categories.find();
 });
